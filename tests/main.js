60c6888f6b86b218e6283b1f1f0d15bd85a94439
Pete Hawkins <pete@phawk.co.uk>
2013-01-22 19:06:53 +0000

    Fix tests require config

--- tests/main.js
+++ tests/main.js
@@ -4,18 +4,9 @@ require.config({
         "jquery": "../libs/jquery/jquery.min",
         "underscore": "../libs/underscore/underscore-amd",
         "backbone": "../libs/backbone/backbone-amd",
-        "hbs": "../libs/require/hbs",
-        "i18nprecompile": "../libs/require/hbs/i18nprecompile",
-        "json2": "../libs/require/hbs/json2",
-        "handlebars": "../libs/handlebars/handlebars",
+        "templates": "../src/templates/templates",
         "chai": "../libs/chai/chai",
-        "src": "../src",
-        "text": "../libs/require/text"
-    },
-    shim: {
-        'handlebars': {
-            exports: 'Handlebars'
-        }
+        "src": "../src"
     },
     urlArgs: "bust=" + (new Date()).getTime() // cache-busting for development
 });
